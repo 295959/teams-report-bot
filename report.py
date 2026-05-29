c2925657406261f7b49c12ba3b323177e7458b76
295959 <alrbgh0131@gmail.com>
2026-05-29 16:37:01 +0900

    Update report.py

--- report.py
+++ report.py
@@ -2,7 +2,7 @@ import requests
 from datetime import date, timedelta
 import holidays
 
-WEBHOOK_URL = "https://ihoban.webhook.office.com/webhookb2/77c1f4c7-6e1a-41a2-bb16-a8d22b28c9fc@204624d4-2187-4615-829e-51e1e5503ef3/IncomingWebhook/50fe9a60960e41bba6b0f0bd4de025cb/c0c15cf7-cf72-467c-9e03-02cb7ad6a256/V2GuXMgp4jp0-aiUcUzAjm130g9YkjmWkDQQFBKtemVO81"
+WEBHOOK_URL = "https://ihoban.webhook.office.com/webhookb2/77c1f4c7-6e1a-41a2-bb16-a8d22b28c9fc@204624d4-2187-4615-829e-51e1e5503ef3/IncomingWebhook/0d8ee7819d6644a89927447b7b5ed681/c0c15cf7-cf72-467c-9e03-02cb7ad6a256/V2JmvzfQGufYpa5aqFZj6L7oobQl9tTcv5zwIdwGR3lh41"
 FILE_LINK = "https://ihoban.sharepoint.com/sites/msteams_c62637/_layouts/15/guestaccess.aspx?share=IQCus3CG0UkGTZbySimHnM8yAcetni83ZPwy6A8TrLlzzoA&e=t38dm8"
 
 kr_holidays = holidays.country_holidays("KR")
