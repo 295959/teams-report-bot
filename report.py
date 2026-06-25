4139080a560e2e166d3adc05582f9010491b8b37
295959 <alrbgh0131@gmail.com>
2026-06-25 13:50:29 +0900

    Update report.py

--- report.py
+++ report.py
@@ -3,8 +3,8 @@ from datetime import date, timedelta
 import holidays
 import time
 
-WEBHOOK_URL = "https://ihoban.webhook.office.com/webhookb2/77c1f4c7-6e1a-41a2-bb16-a8d22b28c9fc@204624d4-2187-4615-829e-51e1e5503ef3/IncomingWebhook/2e4f117c121a4736b568f91eafb57fe4/c0c15cf7-cf72-467c-9e03-02cb7ad6a256/V2kSYLw3hFzi2sUIsTsvIgt10VK6shX99Ir6D4_NyUScM1"
-FILE_LINK = "https://ihoban.sharepoint.com/sites/msteams_c62637/_layouts/15/guestaccess.aspx?share=IQCus3CG0UkGTZbySimHnM8yAcetni83ZPwy6A8TrLlzzoA&e=7Svcir"
+WEBHOOK_URL = "https://ihoban.webhook.office.com/webhookb2/77c1f4c7-6e1a-41a2-bb16-a8d22b28c9fc@204624d4-2187-4615-829e-51e1e5503ef3/IncomingWebhook/d6b42a9b617d48fe936e96a4a0d007eb/c0c15cf7-cf72-467c-9e03-02cb7ad6a256/V2BguZhvkPu2FjNXh-aukWsj5T8NTIEWHM_eoYj_ACE_41"
+FILE_LINK = "https://ihoban.sharepoint.com/sites/msteams_c62637/_layouts/15/guestaccess.aspx?share=IQCus3CG0UkGTZbySimHnM8yAcetni83ZPwy6A8TrLlzzoA&e=t38dm8"
 
 kr_holidays = holidays.country_holidays("KR")
 weekdays = ["월", "화", "수", "목", "금", "토", "일"]
